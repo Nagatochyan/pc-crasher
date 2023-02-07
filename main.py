81ead38b88d67ad0ce98e6f0996dacf76b0e37d6
MaxaM <94958239+Nagatochyan@users.noreply.github.com>
2023-02-07 18:19:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import keyboard
+import mouse
 while True:
-  while keyboard.is_pressed:
-    keyboard.send("a")
+    while keyboard.is_pressed:
+        keyboard.send("a")
+    while mouse.click('left'):
+        keyboard.send("a")
+    while mouse.click('right'):
+        keyboard.send("a")
+    while mouse.click('middle'):
+        keyboard.send("a")
